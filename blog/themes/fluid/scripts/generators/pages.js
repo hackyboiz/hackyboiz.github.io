0965cb0cb64f139584a9930c7bdf2cb9bf0f7b91
j0dev <rig0408@naver.comm>
2020-09-23 20:01:15 +0900

    del links files add author files

--- blog/themes/fluid/scripts/generators/pages.js
+++ blog/themes/fluid/scripts/generators/pages.js
@@ -1,44 +1,44 @@
 /* global hexo */
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // generate 404 page
-if (!fs.existsSync(path.join(hexo.source_dir, '404.html'))) {
-  hexo.extend.generator.register('_404', function(locals) {
+if (!fs.existsSync(path.join(hexo.source_dir, "404.html"))) {
+  hexo.extend.generator.register("_404", function (locals) {
     return {
-      path  : '404.html',
-      data  : locals.theme,
-      layout: '404'
+      path: "404.html",
+      data: locals.theme,
+      layout: "404",
     };
   });
 }
 
 // generate tags Page
-hexo.extend.generator.register('_tags', function(locals) {
+hexo.extend.generator.register("_tags", function (locals) {
   return {
-    path  : 'tags/index.html',
-    data  : locals.theme,
-    layout: 'tags'
+    path: "tags/index.html",
+    data: locals.theme,
+    layout: "tags",
   };
 });
 
 // generate categories Page
-hexo.extend.generator.register('_categories', function(locals) {
+hexo.extend.generator.register("_categories", function (locals) {
   return {
-    path  : 'categories/index.html',
-    data  : locals.theme,
-    layout: 'categories'
+    path: "categories/index.html",
+    data: locals.theme,
+    layout: "categories",
   };
 });
 
-// generate links page
-hexo.extend.generator.register('_links', function(locals) {
+// generate author page
+hexo.extend.generator.register("_author", function (locals) {
   return {
-    path  : 'links/index.html',
-    data  : locals.theme,
-    layout: 'links'
+    path: "author/index.html",
+    data: locals.theme,
+    layout: "author",
   };
 });
